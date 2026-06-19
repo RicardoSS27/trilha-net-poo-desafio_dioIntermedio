aed090d37d1a55214f85e2a8386e52f1de7a5729
RicardoSS27 <ricardosousa2709y@gmail.com>
2026-06-19 15:55:31 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
+
+System.Console.WriteLine("\n");
+
+// Criar uma instância de Iphone
+Iphone iphone = new Iphone("987654321", "iPhone 12", "543210987654321", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
